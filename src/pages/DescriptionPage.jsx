29832e9c929416c13361492331a3b15a86655cc3
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-31 12:41:27 +0200

    feat: ajout des information de la page Description

--- src/pages/DescriptionPage.jsx
+++ src/pages/DescriptionPage.jsx
@@ -4,6 +4,10 @@ const DescriptionPage = () => {
   return (
     <>
       <HeaderNav />
+      <h2>Description</h2>
+      <p>
+        Le jeu compte le nombre de brelan(s) obtenu(s) en laçant 3 de dés non-pipés un nombre de fois à fixer par l'utilisateur.
+      </p>
     </>
   );
 };
