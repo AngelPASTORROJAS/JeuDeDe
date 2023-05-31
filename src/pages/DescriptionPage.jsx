a97a6be8dcc50fd8a1da1d36a7395f16a225aef3
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-31 15:30:27 +0200

    feat: correction des valeurs énnoncées dans CONSIGNES.md

--- src/pages/DescriptionPage.jsx
+++ src/pages/DescriptionPage.jsx
@@ -3,7 +3,7 @@ const DescriptionPage = () => {
     <>
       <h2 style={{ color: "white" }}>Description</h2>
       <p style={{ color: "white" }}>
-        Le jeu compte le nombre de brelan(s) obtenu(s) en laçant 3 de dés
+        Le jeu compte le nombre de brelan(s) obtenu(s) en laçant 5 de dés
         non-pipés un nombre de fois à fixer par l'utilisateur.
       </p>
       <a style={{ textDecoration: "none" }} href="/">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 const HomePage = () => {
   return (
     <>
+    <p style={{ color: "white" }}>
+        Nombre de dé(s)
+        <input type="number" value="5" min="1" />
+      </p>
       <p style={{ color: "white" }}>
         Nombre d'expérience(s)
-        <input type="number" min="1" max="100" />
+        <input type="number" min="1" value="1" />
       </p>
       <button>lancer</button>
       <a style={{ textDecoration: "none" }} href="/statistic">
