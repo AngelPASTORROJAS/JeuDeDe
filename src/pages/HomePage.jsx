b6e386c4b98516775c5291a5530b491ca260bda3
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-31 15:48:22 +0200

    feat: ajout de useReducer pour récuperer le nombre de dé

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,35 @@
+import { useReducer } from "react";
+
+const initialState = {
+  nbDe: 5,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "updateDe":
+      return { ...state, nbDe: action.value };
+  }
+};
+
 const HomePage = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
-    <p style={{ color: "white" }}>
+      <p style={{ color: "white" }}>
         Nombre de dé(s)
-        <input type="number" value="5" min="1" />
+        <input
+          onChange={(e) =>
+            dispatch({ type: "updateDe", value: e.target.value })
+          }
+          type="number"
+          value={state.nbDe}
+          min="1"
+        />
       </p>
+      <p style={{ color: "white" }}>{state.nbDe}</p>
       <p style={{ color: "white" }}>
         Nombre d'expérience(s)
-        <input type="number" min="1" value="1" />
+        <input type="number" min="1" value={state.nbExperience} />
       </p>
       <button>lancer</button>
       <a style={{ textDecoration: "none" }} href="/statistic">
