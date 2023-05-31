0fae466ad604297ca3c9f56661bc3e6e29bb47a3
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-31 13:41:01 +0200

    style: ajout du style du projet

--- src/pages/DescriptionPage.jsx
+++ src/pages/DescriptionPage.jsx
@@ -3,11 +3,24 @@ import HeaderNav from "../component/HeaderNav";
 const DescriptionPage = () => {
   return (
     <>
-      <HeaderNav />
-      <h2>Description</h2>
-      <p>
-        Le jeu compte le nombre de brelan(s) obtenu(s) en laçant 3 de dés non-pipés un nombre de fois à fixer par l'utilisateur.
-      </p>
+      <div
+        style={{
+          padding: "1em",
+          backgroundColor: "#0d0c1c",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <HeaderNav />
+        <h2 style={{color:"white"}}>Description</h2>
+        <p style={{color:"white"}}>
+          Le jeu compte le nombre de brelan(s) obtenu(s) en laçant 3 de dés
+          non-pipés un nombre de fois à fixer par l'utilisateur.
+        </p>
+        <a style={{ textDecoration: "none" }} href="/">
+          <p style={{ color: "white", fontSize: "2em" }}>Home</p>
+        </a>
+      </div>
     </>
   );
 };
