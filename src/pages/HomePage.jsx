b15405691687b9e7595741e745e763324d42a16d
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-31 15:52:23 +0200

    feat: ajout de useReducer pour récuperer le nombre de d'éxrience réaliser

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,14 @@ import { useReducer } from "react";
 
 const initialState = {
   nbDe: 5,
+  nbExperience: 1,
 };
 const reducer = (state, action) => {
   switch (action.type) {
-    case "updateDe":
+    case "updateNbDe":
       return { ...state, nbDe: action.value };
+    case "updateNbExperience":
+      return { ...state, nbExperience: action.value };
   }
 };
 
@@ -19,18 +22,26 @@ const HomePage = () => {
         Nombre de dé(s)
         <input
           onChange={(e) =>
-            dispatch({ type: "updateDe", value: e.target.value })
+            dispatch({ type: "updateNbDe", value: e.target.value })
           }
           type="number"
           value={state.nbDe}
-          min="1"
+          min="3"
         />
       </p>
       <p style={{ color: "white" }}>{state.nbDe}</p>
       <p style={{ color: "white" }}>
         Nombre d'expérience(s)
-        <input type="number" min="1" value={state.nbExperience} />
+        <input
+          onChange={(e) =>
+            dispatch({ type: "updateNbExperience", value: e.target.value })
+          }
+          type="number"
+          min="1"
+          value={state.nbExperience}
+        />
       </p>
+      <p style={{ color: "white" }}>{state.nbExperience}</p>
       <button>lancer</button>
       <a style={{ textDecoration: "none" }} href="/statistic">
         <p style={{ color: "white", fontSize: "2em" }}>Resultat</p>
