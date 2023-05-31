20532954472c9b9f7a4ddfc491e97639f4803058
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-31 12:41:49 +0200

    feat: ajout des information de la HomaPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ const HomePage = () => {
   return (
     <>
       <HeaderNav />
+      <p>
+        Nombre d'expérience(s)
+        <input type="number" min="1" max="100" />
+      </p>
+      <button>lancer</button>
+      <a href="/description">
+        <p>Resultat</p>
+      </a>
     </>
   );
 };
